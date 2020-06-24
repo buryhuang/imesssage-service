8cbd65b8fc6a88234a9cad353652bebcf423ca38
Bury Huang <>
2020-06-24 15:04:50 -0700

    send as group chat

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ import os
 
 app = Flask(__name__)
 
+def generate_send_script(app_number, to_number, msg):
+    f = open("send_message.applescript", "w")
+    f.write("tell application \"Messages\"\n")
+    f.write("    if not running then run\n")
+    f.write("    activate\n")
+    f.write("    set theBuddy1 to buddy \"%s\" of service \"SMS\"\n" % to_number)
+    f.write("    set theBuddy2 to buddy \"%s\" of service \"SMS\"\n" % app_number)
+    f.write("    set thisChat to make new text chat with properties {participants:{theBuddy1, theBuddy2}}\n")
+    f.write("    set thisMessage to send \"%s\" to thisChat\n" % msg)
+    f.write("end tell\n")
+    f.close
 
 @app.route('/imessage', methods = ['GET', 'POST'])
 def handle_imesssage():
@@ -12,7 +23,8 @@ def handle_imesssage():
     if request.method == 'POST':
         data = request.form
         print("sending %s to %s" % (data.get('video-url'), data.get('receiver_number')))
-        result = os.system('/usr/bin/osascript -e \'tell application \"Messages\" to send \"From Bury %s\" to buddy \"%s\" of service \"SMS\"\'' % (data.get('video-url'), data.get('receiver_number')))
+        generate_send_script("+12038942437", data.get('receiver_number'), data.get('msg'))
+        result = os.system('/usr/bin/osascript send_message.applescript')
     return "Executed! with %d" % result
 
 if __name__ == '__main__':

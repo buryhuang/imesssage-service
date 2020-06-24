f5020c380148497940dee130c713ce7b2c30e147
Bury Huang <>
2020-06-24 12:34:54 -0700

    handle post with input parameters

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import request
+
+import os
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/imessage', methods = ['GET', 'POST'])
+def handle_imesssage():
+    result = 167
+    if request.method == 'POST':
+        data = request.form
+        print("sending %s to %s" % (data.get('video-url'), data.get('receiver_number')))
+        result = os.system('/usr/bin/osascript -e \'tell application \"Messages\" to send \"From Bury %s\" to buddy \"%s\" of service \"SMS\"\'' % (data.get('video-url'), data.get('receiver_number')))
+    return "Executed! with %d" % result
 
 if __name__ == '__main__':
     app.run()
